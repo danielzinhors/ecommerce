4a41ab3342d8528ed82394f03f793ef0b3890d83
Daniel <danielzinhors@gmail.com>
2019-08-14 21:30:36 -0300

    Paginacao da categoria

--- site.php
+++ site.php
@@ -13,7 +13,7 @@ $app->get('/', function() {
 
 });
 
-$app->get('/categories/:idcategory', function($idcategory){
+/*$app->get('/categories/:idcategory', function($idcategory){
 
     $category = new Category;
     $category->get((int)$idcategory);
@@ -22,6 +22,35 @@ $app->get('/categories/:idcategory', function($idcategory){
 				array(
 					"category" => $category->getValues(),
 					"products" => Product::checkList($category->getProducts())
+				)
+		);
+});*/
+
+$app->get('/categories/:idcategory', function($idcategory){
+
+		$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    $category = new Category;
+    $category->get((int)$idcategory);
+
+		$pagination = $category->getProductsPage($page);
+
+		$pages = [];
+
+		for ($i = 1; $i <= $pagination['pages']; $i++){
+				array_push(
+						$pages,
+						array(
+							'link' => '/categories/' . $category->getidcategory() . '?page=' . $i,
+						  'page' => $i
+						)
+				);
+		}
+
+    chamaTpl("category",
+				array(
+					"category" => $category->getValues(),
+					"products" => $pagination['data'],
+					"pages" => $pages
 				)
 		);
 });
