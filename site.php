e47ced4de1e124776465e74e5135b0f4c051562d
danielzinhors <danielzinhors@gmail.com>
2019-09-22 17:12:59 -0300

    Ajuste css

--- site.php
+++ site.php
@@ -4,16 +4,6 @@ use \Berinc\Model\Product;
 
 $app->get('/', function() {
 	
-	chamaTpl("index2", 
-		array(),
-		false,
-		false
-	);
-
-});
-
-$app->get('/dev', function() {
-	
 	$products = Product::listAll();
 	
 	chamaTpl("index", array(
