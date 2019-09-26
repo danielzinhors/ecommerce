2446b6eae74724c11148bac18408947c22eb4ca6
daniel <danielzinhors@gmail.com>
2019-09-26 20:45:17 -0300

    pag products

--- site-products.php
+++ site-products.php
@@ -8,11 +8,25 @@ $app->get('/products/:desurl', function($desurl){
 		$product = new Product();
 		$product->getFromURL($desurl);
 
-		chamaTpl("product-detail", array(
-						'product' => $product->getValues(),
-						'categories' => $product->getCategoies()
-			  )
+		chamaTpl("product-detail", 
+			array(
+				'product' => $product->getValues(),
+				'categories' => $product->getCategoies()
+			)
 		);
+
+});
+
+$app->get('/products', function(){
+
+	$products = Product::listAll();
+
+	chamaTpl("products", 
+		array(
+			"products" => Product::checkList($products)
+			//'categories' => $product->getCategoies()
+		)
+	);
 
 });
 
