4dbb7e8852601b8845962eca9f876e8c0e8e707c
daniel <danielzinhors@gmail.com>
2019-10-24 21:14:08 -0300

    cc

--- vendor/berinc/php-classes/src/DB/Config.php
+++ vendor/berinc/php-classes/src/DB/Config.php
@@ -5,7 +5,7 @@ namespace Berinc\DB;
 class Config {
 	
 	const HOSTNAME = "127.0.0.1";
-	const USERNAME = "wordpress_user";
-	const PASSWORD = "Dadosbr@";
-    const DBNAME = "db_ecommerce";
+	const USERNAME = "u286383109_favo";
+	const PASSWORD = "Rg11334711@";
+    const DBNAME = "u286383109_lafavorita";
 }
